5676aa7347f576774701e442fb88e3c755cddb05
Evan Straw <evan.straw99@gmail.com>
2019-08-13 23:59:56 -0700

    Add update for youtube-dl

---
+++ updates/packages/youtube-dl.scm
@@ -0,0 +1,49 @@
+(define-module (updates packages youtube-dl)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system python)
+  #:use-module (guix utils)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define-public youtube-dl
+  (package
+    (name "youtube-dl")
+    (version "2019.08.13")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://github.com/rg3/youtube-dl/releases/"
+                                  "download/" version "/youtube-dl-"
+                                  version ".tar.gz"))
+              (sha256
+               (base32
+                "03a729198zzhixbn7xixi9bprmk3qqh5dsyh3dqhji6lmlijx1w5"))))
+    (build-system python-build-system)
+    (arguments
+     ;; The problem here is that the directory for the man page and completion
+     ;; files is relative, and for some reason, setup.py uses the
+     ;; auto-detected sys.prefix instead of the user-defined "--prefix=FOO".
+     ;; So, we need pass the prefix directly.  In addition, make sure the Bash
+     ;; completion file is called 'youtube-dl' rather than
+     ;; 'youtube-dl.bash-completion'.
+     `(#:tests? #f ; Many tests fail. The test suite can be run with pytest.
+       #:phases (modify-phases %standard-phases
+                  (add-before 'install 'fix-the-data-directories
+                    (lambda* (#:key outputs #:allow-other-keys)
+                      (let ((prefix (assoc-ref outputs "out")))
+                        (mkdir "bash-completion")
+                        (rename-file "youtube-dl.bash-completion"
+                                     "bash-completion/youtube-dl")
+                        (substitute* "setup.py"
+                          (("youtube-dl\\.bash-completion")
+                           "bash-completion/youtube-dl")
+                          (("'etc/")
+                           (string-append "'" prefix "/etc/"))
+                          (("'share/")
+                           (string-append "'" prefix "/share/")))
+                        #t))))))
+    (synopsis "Download videos from YouTube.com and other sites")
+    (description
+     "Youtube-dl is a small command-line program to download videos from
+YouTube.com and many more sites.")
+    (home-page "https://yt-dl.org")
+    (license license:public-domain)))
