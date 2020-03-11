37ed82100e88767ba0280ca36d30c97b1387b0c5
Evan Straw <evan.straw99@gmail.com>
2020-03-10 23:01:02 -0700

    Modify guile-websocket package to support Guile 3.0

--- non-gnu/packages/guile-websocket.scm
+++ non-gnu/packages/guile-websocket.scm
@@ -18,6 +18,7 @@
             (uri (git-reference
                   (url "git://dthompson.us/guile-websocket.git")
                   (commit "d58d696")))
+	    (file-name (git-file-name name version))
             (sha256
              (base32
               "10r8384frlyrljjdyzilrh8hzi60v9hisx4fxjs7rmg9g01cs77k"))))
@@ -26,15 +27,21 @@
     '(#:make-flags
       '("GUILE_AUTO_COMPILE=0")
       #:phases
-      (modify-phases
-       %standard-phases
-       (add-after 'unpack 'bootstrap
-		  (lambda _ (zero? (system* "sh" "bootstrap")))))))
+      (modify-phases %standard-phases
+	(add-after 'unpack 'update-guile-version
+	  (lambda _
+	    (substitute* "configure.ac"
+	      (("2.0.9") "3.0.0"))
+	    (substitute* "Makefile.am"
+	      (("2.0") "3.0"))
+	    #t))
+	(add-after 'update-guile-version 'bootstrap
+	  (lambda _ (zero? (system* "sh" "bootstrap")))))))
    (native-inputs
     `(("autoconf" ,autoconf)
       ("automake" ,automake)))
    (inputs
-    `(("guile" ,guile-2.0)))
+    `(("guile" ,guile-next)))
    (synopsis "Websocket server/client for Guile")
    (description "Guile-websocket provides an implementation of the
 WebSocket protocol as defined by RFC 6455.")
