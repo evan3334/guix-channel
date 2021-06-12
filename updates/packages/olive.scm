d6e460c369c6ae620c03653779b57517339cef3b
Evan Straw <evan.straw99@gmail.com>
2021-06-11 18:40:57 -0700

    Update Olive to commit 48828d66

--- updates/packages/olive.scm
+++ updates/packages/olive.scm
@@ -4,8 +4,10 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system cmake)
   #:use-module (guix build-system gnu)
+  #:use-module (guix utils)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages autotools)
+  #:use-module (gnu packages compression)
   #:use-module (gnu packages qt)
   #:use-module (gnu packages ghostscript)
   #:use-module (gnu packages video)
@@ -16,7 +18,8 @@
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages serialization)
   #:use-module (gnu packages xml)
-  #:use-module (gnu packages version-control))
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages web))
 
 (define-public pystring
   (package
@@ -135,11 +138,13 @@ many popular formats.")
     (build-system cmake-build-system)
     (arguments `(#:configure-flags '("-DPYTHON=ON")))
     (home-page "https://github.com/AcademySoftwareFoundation/Imath")
-    (synopsis "C++ and Python library of 2D and 3D vector, matrix, and math operations for computer graphics")
+    (synopsis "C++ and Python library of 2D and 3D vector, matrix, and math operations for
+computer graphics")
     (description "Imath is a basic, light-weight, and efficient C++ representation of 2D and 3D
 vectors and matrices and other simple but useful mathematical objects, functions, and data
 types common in computer graphics applications, including the “half” 16-bit floating-point
-type.  Imath also includes optional python bindings for all types and functions, including optimized implementations of vector and matrix arrays.")
+type.  Imath also includes optional python bindings for all types and functions, including
+optimized implementations of vector and matrix arrays.")
     (license license:bsd-3)))
 
 (define-public openexr-3
@@ -178,7 +183,8 @@ type.  Imath also includes optional python bindings for all types and functions,
     (home-page "https://www.openexr.com")
     (synopsis "High-dynamic range file format library")
     (description "OpenEXR is a high dynamic-range (HDR) image file format developed for use in
-computer imaging applications.  The IlmImf C++ libraries support storage of the \"EXR\" file format for storing 16-bit floating-point images.")
+computer imaging applications.  The IlmImf C++ libraries support storage of the \"EXR\" file
+format for storing 16-bit floating-point images.")
     (license license:bsd-3)))
 
 (define-public optional-lite
@@ -200,32 +206,126 @@ computer imaging applications.  The IlmImf C++ libraries support storage of the 
     (description "optional lite is a single-file header-only library to represent optional
 (nullable) objects and pass them by value.  The library aims to provide a C++17-like optional for use with C++98 and later.")
     (license license:boost1.0)))
+
+(define-public cpp11-any
+  (let ((version "0.0.0")
+	(revision "1")
+	(commit "f67bd5f8bbf7eb628bf38206d4ac5cb22438e6bb"))
+    (package
+      (name "cpp11-any")
+      (version (git-version version revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/thelink2012/any")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1ch6aq363dqx6bx7yjg4lkq346sy2acw1fnamg4jdqs4z8rqz9jz"))))
+      (build-system cmake-build-system)
+      (home-page "https://github.com/thelink2012/any")
+      (synopsis "Implementation of std::experimental::any, including small object optimization, for C++11 compilers")
+      (description "This is a implementation of N4562 std::experimental::any (merged into
+C++17) for C++11 compilers.
+
+It contains a small object optimization for objects with a size of up to 2 words (such as int,
+float and std::shared_ptr). Storing those objects in the container will not trigger a dynamic
+allocation.")
+      (license license:boost1.0))))
+
+;; TODO: actually get opentimelineio to work (had a lot of issues with bundled dependencies;
+;; version of rapidjson that it wants is not the one packaged in Guix)
+
+;;*  (define-public opentimelineio
+;;*    (package
+;;*      (name "opentimelineio")
+;;*      (version "0.13")
+;;*      (source (origin
+;;*                (method git-fetch)
+;;*                (uri (git-reference
+;;*                      (url "https://github.com/PixarAnimationStudios/OpenTimelineIO")
+;;*                      (commit (string-append "v" version))
+;;*                      (recursive? #t)))
+;;*                (file-name (git-file-name name version))
+;;*                (sha256
+;;*                 (base32
+;;*                  "136an91rd4ympsg363rgd5h0a54ps072pjd9hbmrad685rcjmmzn"))
+;;*                (modules '((guix build utils)))
+;;*                (snippet
+;;*                 '(begin
+;;*                    (for-each delete-file-recursively
+;;*                              '("src/deps/optional-lite"
+;;*                                "src/deps/pybind11"
+;;*                                "src/deps/rapidjson"
+;;*                                "src/macosx-build"))
+;;*                    #t))))
+;;*      (build-system cmake-build-system)
+;;*      (arguments
+;;*       `(#:phases
+;;*         (modify-phases %standard-phases
+;;*           (add-after 'unpack 'fix-bundled-deps
+;;*             (lambda* (#:key inputs #:allow-other-keys)
+;;*               (let ((rapidjson (assoc-ref inputs "rapidjson"))
+;;*                     (optional-lite (assoc-ref inputs "optional-lite")))
+;;*                 (substitute* "src/opentimelineio/CMakeLists.txt"
+;;*                   (("\\$\\{PROJECT_SOURCE_DIR\\}/src/deps/rapidjson") rapidjson)
+;;*                   (("\\$\\{PROJECT_SOURCE_DIR\\}/src/deps/optional-lite") optional-lite))
+;;*                 (substitute* "src/deps/CMakeLists.txt"
+;;*                   (("add_subdirectory\\(pybind11\\)") "")
+;;*                   (("DEPS_SUBMODULES any optional-lite pybind11 rapidjson")
+;;*                    "DEPS_SUBMODULES any")
+;;*                   (("optional-lite/")
+;;*                    (string-append optional-lite "/")))
+;;*                 #t))))))
+;;*      (inputs `(("pyside2" ,python-pyside-2)
+;;*                ("pybind11" ,pybind11)
+;;*                ("rapidjson" ,rapidjson)
+;;*                ("optional-lite" ,optional-lite)))
+;;*      (home-page "https://opentimeline.io")
+;;*      (synopsis "API and interchange format for editorial timeline information")
+;;*      (description "OpenTimelineIO is an interchange format and API for editorial cut
+;;*  information.  OTIO is not a container format for media, rather it contains information about
+;;*  the order and length of cuts and references to external media.
+
+;;*  OTIO includes both a file format and an API for manipulating that format.  It also includes a
+;;*  plugin architecture for writing adapters to convert from/to existing editorial timeline
+;;*  formats.  It also implements a dependency-less library for dealing strictly with time,
+;;*  opentime.")
+;;*      (license `((license:fsf-free "file://LICENSE.txt" "Modified Apache 2.0 License")
+;;*                 license:boost1.0)))) ;; "any" library in src/deps is under BSL1.0
+
 (define-public olive-editor
   (let ((version "0.2.0")
-		(revision "1")
-		(commit "2f4e9809ceeba5f43583e6c923774d990c49be59"))
+	(revision "1")
+	(commit "48828d6632a126c0967368eefd7114b9acebb076"))
     (package
       (name "olive-editor")
       (version (git-version version revision commit))
       (source (origin
-				(method git-fetch)
-				(uri (git-reference
-					  (url "https://github.com/olive-editor/olive")
-					  (commit commit)))
-				(file-name (git-file-name name version))
-				(sha256
-				 (base32
-				  "1sfzibjrmwr1b00a26zjkiq2wa3cbibq7bs0sq3a9z10h489w166"))))
+		(method git-fetch)
+		(uri (git-reference
+		      (url "https://github.com/olive-editor/olive")
+		      (commit commit)))
+		(file-name (git-file-name name version))
+		(sha256
+		 (base32
+		  "1qi1zvppmlfw789j48g9kk9jxxasja1gbv4kjm53dmndmqga8gmr"))))
       (build-system cmake-build-system)
-      (arguments `(#:tests? #f))
+      (arguments
+       `(#:configure-flags '("-DCMAKE_CXX_FLAGS=-Wno-error=shadow")))
       (inputs `(("qtbase" ,qtbase)
-				("qtmultimedia" ,qtmultimedia)
-				("qttools" ,qttools)
-				("qtsvg" ,qtsvg)
-				("ffmpeg" ,ffmpeg)
-				("opencolorio" ,opencolorio-2)
-				("openimageio" ,openimageio)
-				("openexr" ,openexr)))
+		("qtmultimedia" ,qtmultimedia)
+		("qttools" ,qttools)
+		("qtsvg" ,qtsvg)
+		("ffmpeg" ,ffmpeg)
+		("opencolorio" ,opencolorio-2)
+		("openimageio" ,openimageio)
+                ("zlib" ,zlib)
+                ("imath" ,imath)
+		("openexr" ,openexr-3)
+                ("ilmbase" ,ilmbase)))
+      (native-inputs `(("pkg-config" ,pkg-config)))
       (home-page "https://olivevideoeditor.org")
       (synopsis "Free non-linear video editor")
       (description #f)
